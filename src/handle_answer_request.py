929e0e1c3d50ef9ba29d489f71aef1099cf0c4b7
Josh Campbell <jcampbell@zillowgroup.com>
2020-08-03 13:20:13 -0700

    refactor :fire:

--- src/handle_answer_request.py
+++ src/handle_answer_request.py
@@ -5,149 +5,111 @@ from __future__ import print_function
 import logging
 from fuzzywuzzy import fuzz
 from alexa_responses import speech_with_card, speech
-from manage_data import update_score_and_games, update_score_games_and_pack
-from strings import (
-    WRONG_ANSWER_CLUES_REMAIN, NEXT_CLUE, random_correct_answer_message,
-    WRONG_ANSWER, END_GAME_WRAP_UP, NO_MORE_CLUES
-)
+from manage_data import update_dynamodb
+import strings
 from word_bank import CURRENT_PACK_ID
+from session_attributes import SessionAttributes
 
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-def handle_answer_request(intent, session):
+def handle_answer_request(intent, session_attributes):
     """ Check if the answer is right, adjust score, and continue """
     logger.debug("=====handle_answer_request fired...")
-    # Setup.
-    attributes = {}
-    answer = get_answer_from_(intent)
-    questions = session['attributes']['questions']
-    game_length = session['attributes']['game_length']
-    current_score = session['attributes']['score']
-    current_question_index = session['attributes']['current_question_index']
-    current_clue_index = session['attributes']['current_clue_index']
-    player_info = session['attributes']['player_info']
-    play_newest_word_pack = session['attributes']['play_newest_word_pack']
-    current_question_value = 50 - int((current_clue_index * 10))
-    correct_answer = questions[current_question_index]['code_word'].lower()
-    answered_correctly = None
+    logger.debug(session_attributes)
+
+    this_game = SessionAttributes(session_attributes)
+    answer_heard = get_answer_from_(intent)
+    current_question_value = 50 - int(this_game.current_clue_index * 10)
+    correct_answer = this_game.get_answer_for_current_question()
+
     # Use Levenshtein distance algo to see if the words are similar enough.
-    fuzzy_score = fuzz.partial_ratio(answer, correct_answer)
+    fuzzy_score = fuzz.partial_ratio(answer_heard, correct_answer)
 
     # Currently using a hardcoded score of 60 or better.
-    if correct_answer in answer or fuzzy_score >= 60:
-        current_score += current_question_value
+    if correct_answer in answer_heard or fuzzy_score >= 60:
+        this_game.update_total_score(
+            current_question_value + this_game.total_score)
         answered_correctly = True
     else:
-        log_wrong_answer(answer, correct_answer)
+        log_wrong_answer(answer_heard, correct_answer)
         answered_correctly = False
         # If clues remain give them the next clue instead of moving on.
-        if current_clue_index != 4:
-            return next_clue_request(session=session, was_wrong_answer=True)
+        if this_game.current_clue_index != 4:
+            return next_clue_request(attributes=this_game, last_guess_was_wrong=True)
 
-    # If that was the last word end the game.
-    if current_question_index == game_length - 1:
-        if play_newest_word_pack:
-            player_info['lastWordPackPlayed']['N'] = CURRENT_PACK_ID
-        if answered_correctly:
-            current_score += current_question_value
-        return end_game_return_score(current_score, player_info, answered_correctly,
-                                     answer, correct_answer, play_newest_word_pack)
+    # If that was the last word and no clues remain, end the game.
+    if this_game.current_question_index == this_game.game_length - 1:
+        # If this was the latest word pack mark it
+        # as played so the player doesn't get it again.
+        if this_game.play_newest_word_pack:
+            this_game.update_last_word_pack_played(CURRENT_PACK_ID)
+
+        return end_game_return_score(this_game, answered_correctly,
+                                     answer_heard, correct_answer)
 
     # If that wasn't the last word in the game continue on to next word.
-    current_question_index += 1
-    speech_output = "Get ready.  Next round in 3... 2... 1... " +\
-        questions[current_question_index]['clues'][0]
-    attributes = {
-        "current_question_index": current_question_index,
-        "current_clue_index": 0,
-        "questions": questions,
-        "score": current_score,
-        "game_length": game_length,
-        "game_status": "in_progress",
-        "current_clue": questions[current_question_index]['clues'][0],
-        "player_info": player_info,
-        "play_newest_word_pack": play_newest_word_pack
-    }
+    this_game.move_on_to_next_question()
+    speech_output = strings.NEXT_ROUND_WITH_CLUE.format(
+        this_game.get_first_clue())
 
     if answered_correctly:
-        speech_output = random_correct_answer_message(
-            str(correct_answer)) + speech_output
+        speech_output = strings.random_correct_answer_message(
+            correct_answer) + speech_output
         card_text = "The word was:  " + correct_answer + ". You got " + \
             str(current_question_value) + " points!"
         card_title = "You figured out the word!"
     else:
-        speech_output = WRONG_ANSWER.format(
-            str(correct_answer)) + speech_output
+        speech_output = strings.WRONG_ANSWER.format(
+            correct_answer) + speech_output
         card_text = "The word was:  " + correct_answer + "\n" + \
-            "You said:  " + str(answer)
+            "You said:  " + str(answer_heard)
         card_title = "That wasn't the word!"
 
     return speech_with_card(tts=speech_output,
-                            attributes=attributes,
+                            attributes=this_game.attributes,
                             should_end_session=False,
                             card_title=card_title,
                             card_text=card_text,
                             answered_correctly=answered_correctly)
 
 
-def end_game_return_score(current_score, player_info, answered_correctly,
-                          answer, correct_answer, play_newest_word_pack):
-    """ If the customer answered the last question end the game """
+def end_game_return_score(this_game, answered_correctly,
+                          answer_heard, correct_answer):
+    """ If the customer answered the last question we end the game """
     logger.debug("=====end_game_return_score fired...")
-    wrap_up = END_GAME_WRAP_UP.format(str(current_score))
+    this_game.increment_total_games_played()
+    update_dynamodb(this_game.get_customer_id(), this_game.player_info)
+
+    wrap_up_speech = strings.END_GAME_WRAP_UP.format(
+        str(this_game.total_score))
 
     if answered_correctly:
-        speech_output = random_correct_answer_message(
-            str(correct_answer)) + wrap_up
-        card_text = "Your score is " + str(current_score) + " points!\n" + \
+        speech_output = strings.random_correct_answer_message(
+            correct_answer) + wrap_up_speech
+        card_text = "Your score is " + str(this_game.total_score) + " points!\n" + \
             "The last word was: " + correct_answer
     else:
-        speech_output = WRONG_ANSWER.format(
-            str(correct_answer)) + wrap_up
-        card_text = "Your score is " + str(current_score) + " points!\n" + \
-            "\nThe last word was: " + correct_answer + "\nYou said: " + answer
+        speech_output = strings.WRONG_ANSWER.format(
+            str(correct_answer)) + wrap_up_speech
+        card_text = "Your score is " + str(this_game.total_score) + " points!\n" + \
+            "\nThe last word was: " + correct_answer + "\nYou said: " + answer_heard
 
-    customer_id = player_info['customerID']['S']
-    new_lifetime_score = int(current_score) + \
-        int(player_info['lifetimeScore']['N'])
-    new_games_played = 1 + int(player_info['gamesPlayed']['N'])
-    last_word_pack_played = player_info['lastWordPackPlayed']['N']
-    should_end_session = False
-    attributes = {
-        "game_status": "ended",
-        "player_info": {
-            'customerID': {
-                'S': customer_id
-            },
-            'lifetimeScore': {
-                'N': str(new_lifetime_score)
-            },
-            'gamesPlayed': {
-                'N': str(new_games_played)
-            },
-            'lastWordPackPlayed': {
-                'N': str(last_word_pack_played)
-            }
-        }
-    }
-
-    if play_newest_word_pack:
-        update_score_games_and_pack(customer_id, new_lifetime_score, new_games_played,
-                                    last_word_pack_played, current_score)
-    else:
-        update_score_and_games(
-            customer_id, new_lifetime_score, new_games_played, current_score)
-
-    card_title = "Game Results"
+    card_title = "Clue Countdown Results"
     reprompt = "Would you like to play Clue Countdown again?"
-    return speech_with_card(speech_output, attributes, should_end_session,
-                            card_title, card_text, answered_correctly, reprompt)
+
+    return speech_with_card(tts=speech_output,
+                            attributes=this_game.attributes,
+                            should_end_session=False,
+                            card_title=card_title,
+                            card_text=card_text,
+                            answered_correctly=answered_correctly,
+                            reprompt=reprompt)
 
 
-def next_clue_request(session=None, was_wrong_answer=None):
+def next_clue_request(attributes=None, last_guess_was_wrong=None):
     """ Give player the next clue """
     logger.debug("=====next_clue_request fired...")
     attributes = {}
