6a7356e9531d6278d5de131e749e4cf3c71c35c4
Josh Campbell <jcampbell@zillowgroup.com>
2020-08-03 13:19:49 -0700

    adds additional getters and setters

--- src/session_attributes.py
+++ src/session_attributes.py
@@ -10,69 +10,48 @@ class SessionAttributes():
         self.player_info = None
         self.questions = None
         self.game_length = None
-        self.score = None
+        self.total_score = None
         self.current_clue_index = None
         self.play_newest_word_pack = None
+        self.current_score = None
+        self.current_question_index = None
         self.parse_attributes()
-
-    @property
-    def attributes(self):
-        return self._attributes
-
-    @attributes.setter
-    def attributes(self, attributes):
-        self._attributes = attributes
-        self.parse_attributes()
-
-    @property
-    def game_status(self):
-        return self._game_status
-
-    @game_status.setter
-    def game_status(self, game_status):
-        self._game_status = game_status
-
-    @property
-    def player_info(self):
-        return self._player_info
-
-    @player_info.setter
-    def player_info(self, player_info):
-        self._player_info = player_info
-
-    @property
-    def questions(self):
-        return self._questions
-
-    @questions.setter
-    def questions(self, questions):
-        self._questions = questions
-
-    @property
-    def score(self):
-        return self._score
-
-    @score.setter
-    def score(self, score):
-        self._score = score
-
-    @property
-    def current_clue_index(self):
-        return self._current_clue_index
-
-    @current_clue_index.setter
-    def current_clue_index(self, current_clue_index):
-        self._current_clue_index = current_clue_index
-
-    @property
-    def play_newest_word_pack(self):
-        return self._play_newest_word_pack
-
-    @play_newest_word_pack.setter
-    def play_newest_word_pack(self, play_newest_word_pack):
-        self._play_newest_word_pack = play_newest_word_pack
 
     def parse_attributes(self):
         for key, value in self.attributes.items():
-            print(key, value)
             setattr(self, key, value)
+
+    def get_answer_for_current_question(self):
+        return self.questions[self.current_question_index]['code_word'].lower()
+
+    def update_last_word_pack_played(self, pack_num):
+        player_info = self.player_info
+        player_info['lastWordPackPlayed']['N'] = pack_num
+        setattr(self, 'player_info', player_info)
+
+    def update_total_score(self, total_score):
+        setattr(self, 'total_score', total_score)
+
+        player_info = self.player_info
+        new_lifetime_score = int(
+            player_info['lifetimeScore']['N']) + total_score
+        player_info['lifetimeScore']['N'] = new_lifetime_score
+        player_info['lastScore']['N'] = total_score
+
+    def move_on_to_next_question(self):
+        setattr(self, 'current_question_index',
+                self.current_question_index + 1)
+        setattr(self, 'current_clue_index', 0)
+        setattr(self, 'current_clue',
+                self.questions[self.current_question_index]['clues'][0])
+
+    def get_first_clue(self):
+        return self.questions[self.current_question_index]['clues'][0]
+
+    def get_customer_id(self):
+        return self.player_info['customerID']['S']
+
+    def increment_total_games_played(self):
+        player_info = self.player_info
+        games_played = int(player_info['gamesPlayed']['N']) + 1
+        player_info['gamesPlayed']['N'] = games_played
