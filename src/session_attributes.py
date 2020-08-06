e76de25df429932643c07672586dd317ffe62537
Josh Campbell <josh@p4a.net>
2020-08-06 13:56:17 -0700

    we will just update lastScore on game over

--- src/session_attributes.py
+++ src/session_attributes.py
@@ -33,7 +33,6 @@ class SessionAttributes():
 
         setattr(self, 'total_score', new_total_score)
         self.player_info['lifetimeScore']['N'] = new_lifetime_score
-        self.player_info['lastScore']['N'] = self.new_total_score
 
     def move_on_to_next_word(self):
         """ Update appropriate attributes when moving to the next word """
